caf03ffc0c0c1ce758491406717f04e42cba82be
IgorKohan <nettvor@gmail.com>
2020-08-25 09:50:27 +0200

    .

---
+++ src/examples/Grids.jl
@@ -0,0 +1,444 @@
+using Random
+
+# D. Lazzaro, L. Montefusco, Radial basis functions for the multivariate interpolation of large scattered data sets, J.Comput. Appl. Math., No.140, 2002
+# https://core.ac.uk/download/pdf/82502771.pdf
+# Return half the distance between the closest pair of nodes
+function get_separation_distance(nodes::Matrix{Float64})
+    n = size(nodes, 1)
+    n_1 = size(nodes, 2)
+    sd = sqrt(n) # main hypercube diagonal length
+    @inbounds for i = 1:n_1
+        i1 += 1
+        for j = i1:n_1
+            s = norm(nodes[:,i] .- nodes[:,j])
+            if s < sd
+               sd = s
+           end
+        end
+    end
+    return sd/2.0
+end
+
+# D. Lazzaro, L. Montefusco, Radial basis functions for the multivariate interpolation of large scattered data sets, J.Comput. Appl. Math., No.140, 2002
+# https://core.ac.uk/download/pdf/82502771.pdf
+# Return the radius of the largest inner empty sphere
+function get_fill_distance(nodes::Matrix{Float64}, grid::Matrix{Float64})
+    n = size(nodes, 1)
+    n_1 = size(nodes, 2)
+    ng = size(grid, 2)
+    fd = 0.0
+    @inbounds for i = 1:ng
+        sd = sqrt(T(n))
+        for j = 1:n_1
+            s = norm(grid[:,i] .- nodes[:,j])
+            if s < sd
+               sd = s
+           end
+        end
+        if fd < sd
+           fd = sd
+       end
+    end
+    return fd
+end
+
+########### 1D grids
+
+function get_1D_grid(m::Int)
+    return collect(range(1.3, 1.4; step = 0.1/m))
+end
+
+function get_1D_eps_grid(m::Int)
+    x = collect(range(1.3, 1.4; step = 0.1/m))
+    m1 = m + 1
+    Random.seed!(123)
+    eps = 0.1/m
+    for i = 1:m1
+        x[i] = x[i] + eps * (rand() - 0.5)
+        x[i] = x[i] < 1.0 ? 1.0 : x[i]
+        x[i] = x[i] > 2.0 ? 2.0 : x[i]
+    end
+    return x
+end
+
+function get_1D_halton_nodes(m::Int)
+    x0 = [1.3; 1.4]
+    if m <= 2
+        return x0
+    end
+    m -= 2
+    x = Vector{Float64}(undef, m)
+    reset_halton()
+    init_halton(2)
+    for i = 1:m
+        x[i] = x0[1] + (x0[2] - x0[1]) * get_halton_node()
+    end
+    return [x0; x]
+end
+
+########### 2D grids
+
+function get_2D_grid(m::Int)
+    x = collect(range(0.0, 1.0; step = 1.0/m))
+    y = collect(range(0.0, 1.0; step = 1.0/m))
+    m1 = m + 1
+    ms = m1^2
+    mat = Matrix{Float64}(undef, 2, ms)
+    for i = 1:m1
+        for j = 1:m1
+            r = (i - 1) * m1 + j
+            mat[1, r] = x[i]
+            mat[2, r] = y[j]
+        end
+    end
+    return mat
+end
+
+function get_2D_grid2(m::Int)
+    x = collect(range(-1.0, 1.0; step = 2.0/m))
+    y = collect(range(-1.0, 1.0; step = 2.0/m))
+    m1 = m + 1
+    ms = m1^2
+    mat = Matrix{Float64}(undef, 2, ms)
+    for i = 1:m1
+        for j = 1:m1
+            r = (i - 1) * m1 + j
+            mat[1, r] = x[i]
+            mat[2, r] = y[j]
+        end
+    end
+    return mat
+end
+
+function get_2D_grid3(m::Int)
+    # Rosenbrock function grid
+    #  (x,y) \in [-2;2]x[-1;3]
+    x = collect(range(-2.0, 2.0; step = 4.0/m))
+    y = collect(range(-1.0, 3.0; step = 4.0/m))
+    m1 = m + 1
+    ms = m1^2
+    mat = Matrix{Float64}(undef, 2, ms)
+    for i = 1:m1
+        for j = 1:m1
+            r = (i - 1) * m1 + j
+            mat[1, r] = x[i]
+            mat[2, r] = y[j]
+        end
+    end
+    return mat
+end
+
+function get_2D_eps_grid(m::Int)
+    x = collect(range(0.0, 1.0; step = 1.0/m))
+    y = collect(range(0.0, 1.0; step = 1.0/m))
+    m1 = m + 1
+    ms = m1^2
+    mat = Matrix{Float64}(undef, 2, ms)
+    Random.seed!(123)
+    eps = 2.0 / (5.0 * m)
+    for i = 1:m1
+        for j = 1:m1
+            r = (i - 1) * m1 + j
+            tx = x[i] + eps * (rand() - 0.5)
+            tx = tx < 0.0 ? 0.0 : tx
+            tx = tx > 1.0 ? 1.0 : tx
+            ty = y[j] + eps * (rand() - 0.5)
+            ty = ty < 0.0 ? 0.0 : ty
+            ty = ty > 1.0 ? 1.0 : ty
+            mat[1, r] = tx
+            mat[2, r] = ty
+        end
+    end
+    return mat
+end
+
+function get_2D_random_grid(m::Int)
+    m1 = m + 1
+    ms = m1^2
+    mat = Matrix{Float64}(undef, 2, ms)
+    rng = MersenneTwister(0);
+    rnd = rand(rng, Float64, (2, ms))
+    m3 = ms÷3
+    m3p1 = m3 + 1
+    m23 = 2*m3
+    m23p1 = m23 + 1
+    for i = 1:m3
+        mat[1, i] = rnd[1, i]
+        mat[2, i] = rnd[2, i]
+    end
+    for i = m3p1:m23
+        mat[1, i] = 0.7 + 0.4*(rnd[1, i] - 0.5)
+        mat[2, i] = 0.4 + 0.4*(rnd[2, i] - 0.5)
+    end
+    for i = m23p1:ms
+        mat[1, i] = 0.02 + 0.3*rnd[1, i]
+        mat[2, i] = 0.05 + 0.3*rnd[2, i]
+    end
+    return mat
+end
+
+function get_2D_rect_grid(m::Int)
+    x = collect(range(-4.0, 4.0; step = 8.0/m))
+    y = collect(range(-2.0, 2.0; step = 4.0/m))
+    n1 = length(y)
+    n2 = length(x)
+    mat = Matrix{Float64}(undef, 2, n1*n2)
+    for i = 1:n2
+        for j = 1:n1
+            r = (i - 1) * n1 + j
+            mat[1, r] = x[i]
+            mat[2, r] = y[j]
+        end
+    end
+    return mat
+end
+
+function get_2D_test1_nodes(m::Int)
+    mat0 = [0.0 0.0; 0.0 1.0; 1.0 0.0; 1.0 1.0]'
+    if m < 1
+        return mat0
+    end
+    m1 = m + 1
+    p = collect(range(1.0/m1, (1.0 - 1.0/m1); step = 1.0/m1))
+    ms = m * 4
+    mat = Matrix{Float64}(undef, 2, ms)
+    for i = 1:m
+        mat[1,i] = 0.0
+        mat[2,i] = p[i]
+    end
+    for i = (m+1):(2*m)
+        mat[1,i] = 1.0
+        mat[2,i] = p[i-m]
+    end
+    for i = (2*m+1):(3*m)
+        mat[1,i] = p[i-2*m]
+        mat[2,i] = 0.0
+    end
+    for i = (3*m+1):(4*m)
+        mat[1,i] = p[i-3*m]
+        mat[2,i] = 1.0
+    end
+    w = hcat(mat0, mat)
+    return hcat([0.5; 0.5], w)
+end
+
+function get_2D_border_nodes(m::Int)
+    mat0 = [0.0 0.0; 0.0 1.0; 1.0 0.0; 1.0 1.0]'
+    if m < 1
+        return mat0
+    end
+    m1 = m + 1
+    p = collect(range(1.0/m1, (1.0 - 1.0/m1); step = 1.0/m1))
+    ms = m * 4
+    mat = Matrix{Float64}(undef, 2, ms)
+    for i = 1:m
+        mat[1,i] = 0.0
+        mat[2,i] = p[i]
+    end
+    for i = (m+1):(2*m)
+        mat[1,i] = 1.0
+        mat[2,i] = p[i-m]
+    end
+    for i = (2*m+1):(3*m)
+        mat[1,i] = p[i-2*m]
+        mat[2,i] = 0.0
+    end
+    for i = (3*m+1):(4*m)
+        mat[1,i] = p[i-3*m]
+        mat[2,i] = 1.0
+    end
+    w = hcat(mat0, mat)
+    return w
+end
+
+"""
+Return Lissajous nodes.
+"""
+function get_2D_Lissajous_nodes(n::Int)
+    n1 = n + 1
+    n_all = 4 * n * n1
+    n_nodes = 2 * n^2 + 4 * n + 1
+    nodes = Matrix{Float64}(undef, 2, n_nodes)
+    tol = sqrt(eps())
+    m = 1
+    for k = 1:n_all
+        tk = π  * (k - 1) / (2.0 * n * n1)
+        x = sin(n * tk)
+        y = sin(n1 * tk)
+        dup = false
+        for i = 1:(m-1)
+            if abs(x - nodes[1,i]) <= tol && abs(y - nodes[2,i]) <= tol
+                dup = true
+                continue
+            end
+        end
+        if !dup
+            nodes[1,m] = x
+            nodes[2,m] = y
+            m += 1
+        end
+    end
+    for k = 1:n_nodes
+        nodes[1,k] = (nodes[1,k] + 1.0) / 2.0
+        nodes[2,k] = (nodes[2,k] + 1.0) / 2.0
+    end
+    return nodes
+end
+
+function get_2D_halton_nodes(m::Int)
+    mat0 = [0.0 0.0; 0.0 1.0; 1.0 0.0; 1.0 1.0]'
+    if m <= 4
+        return Matrix(mat0)
+    end
+    m -= 4
+    x = Vector{Float64}(undef, m)
+    reset_halton()
+    init_halton(2)
+    for i = 1:m
+        x[i] = get_halton_node()
+    end
+    y = Vector{Float64}(undef, m)
+    init_halton(3)
+    for i = 1:m
+        y[i] = get_halton_node()
+    end
+    mat = Matrix{Float64}(undef, 2, m)
+    for i = 1:m
+        mat[1, i] = x[i]
+        mat[2, i] = y[i]
+    end
+    return hcat(mat0, mat)
+end
+
+########### 3D grids
+
+function get_3D_grid(m::Int)
+    x = collect(range(0.0, 1.0; step = 1.0/m))
+    y = collect(range(0.0, 1.0; step = 1.0/m))
+    z = collect(range(0.0, 1.0; step = 1.0/m))
+    m1 = m + 1
+    ms = m1^3
+    mat = Matrix{Float64}(undef, 3, ms)
+    for i = 1:m1
+        for j = 1:m1
+            for k = 1:m1
+                r = ((i - 1) * m1 + (j - 1)) * m1 + k
+                mat[1, r] = x[i]
+                mat[2, r] = y[j]
+                mat[3, r] = z[k]
+            end
+        end
+    end
+    return mat
+end
+
+function get_3D_plot_grid(m::Int)
+    x = collect(range(0.0, 1.0; step = 1.0/m))
+    y = collect(range(0.0, 1.0; step = 1.0/m))
+    z = [0.0; 0.25; 0.5; 0.75; 1.0]
+    mz = length(z)
+    m1 = m + 1
+    ms = mz * m1^2
+    mat = Matrix{Float64}(undef, 3, ms)
+    for i = 1:m1
+        for j = 1:m1
+            for k = 1:mz
+                r = ((i - 1) * m1 + (j - 1)) * mz + k
+                mat[1, r] = x[i]
+                mat[2, r] = y[j]
+                mat[3, r] = z[k]
+            end
+        end
+    end
+    return mat
+end
+
+function get_3D_eps_grid(m::Int)
+    x = collect(range(0.0, 1.0; step = 1.0/m))
+    y = collect(range(0.0, 1.0; step = 1.0/m))
+    z = collect(range(0.0, 1.0; step = 1.0/m))
+    m1 = m + 1
+    ms = m1^3
+    mat = Matrix{Float64}(undef, 2, ms)
+    Random.seed!(123)
+    eps = 0.4 / m
+    for i = 1:m1
+        for j = 1:m1
+            for k = 1:m1
+                r = ((i - 1) * m1 + (j - 1)) * m1 + k
+                tx = x[i] + eps * (rand() - 0.5)
+                tx = tx < 0.0 ? 0.0 : tx
+                tx = tx > 1.0 ? 1.0 : tx
+                ty = y[j] + eps * (rand() - 0.5)
+                ty = ty < 0.0 ? 0.0 : ty
+                ty = ty > 1.0 ? 1.0 : ty
+                tz = z[k] + eps * (rand() - 0.5)
+                tz = tz < 0.0 ? 0.0 : tz
+                tz = tz > 1.0 ? 1.0 : tz
+                mat[1, r] = tx
+                mat[2, r] = ty
+                mat[3, r] = tz
+            end
+        end
+    end
+    return mat
+end
+
+function get_3D_random_grid(m::Int)
+    m1 = m + 1
+    ms = m1^3
+    mat = Matrix{Float64}(undef, 3, ms)
+    rng = MersenneTwister(0);
+    rnd = rand(rng, Float64, (3, ms))
+    m3 = ms÷3
+    m3p1 = m3 + 1
+    m23 = 2*m3
+    m23p1 = m23 + 1
+    for i = 1:m3
+        mat[1, i] = rnd[1, i]
+        mat[2, i] = rnd[2, i]
+        mat[3, i] = rnd[3, i]
+    end
+    for i = m3p1:m23
+        mat[1, i] = 0.7 + 0.4*(rnd[1, i] - 0.5)
+        mat[2, i] = 0.8 + 0.3*(rnd[2, i] - 0.5)
+        mat[3, i] = 0.8 + 0.3*(rnd[3, i] - 0.5)
+    end
+    for i = m23p1:ms
+        mat[1, i] = 0.75 + 0.3*rnd[1, i]
+        mat[2, i] = 0.05 + 0.3*rnd[2, i]
+        mat[3, i] = 0.25 + 0.3*rnd[3, i]
+    end
+    return mat
+end
+
+function get_3D_halton_nodes(m::Int)
+    mat0 = [0.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0; 0.0 1.0 1.0; 1.0 0.0 0.0; 1.0 0.0 1.0; 1.0 1.0 0.0; 1.0 1.0 1.0]'
+    if m <= 8
+        return Matrix(mat0)
+    end
+    m -= 8
+    x = Vector{Float64}(undef, m)
+    reset_halton()
+    init_halton(2)
+    for i = 1:m
+        x[i] = get_halton_node()
+    end
+    y = Vector{Float64}(undef, m)
+    init_halton(3)
+    for i = 1:m
+        y[i] = get_halton_node()
+    end
+    z = Vector{Float64}(undef, m)
+    init_halton(5)
+    for i = 1:m
+        z[i] = get_halton_node()
+    end
+    mat = Matrix{Float64}(undef, 3, m)
+    for i = 1:m
+        mat[1, i] = x[i]
+        mat[2, i] = y[i]
+        mat[3, i] = z[i]
+    end
+    return hcat(mat0, mat)
+end
---
+++ src/examples/Models.jl
@@ -0,0 +1,425 @@
+function get_1D_model1(p::Float64)
+    val = 0.0
+    t = abs(3.0*(1.0 - p) + 1.0)
+    if t > 0.0
+        val = 1.0 + p^2 + log(t)/3.3
+    end
+    return val
+end
+
+function get_1D_model1_grad(p::Float64, tol::Float64=1.e-15)
+    val = 0.0
+    if abs(p - 4.0/3.0) > tol
+        val = 2*p + 1.0/(3.3 * (p - 4.0/3.0))
+    end
+end
+
+function get_2D_model1(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    val = 0.0
+    x = p[1]
+    y = p[2]
+    d = (x - 0.75)^2 + (y - 0.75)^2
+    r2 = 0.04
+    if d <= r2
+        val = 1.0
+    end
+    if x <= 0.25 && x >= 0.05 && y <= 0.25 && y >= 0.05
+        val = 1.0
+    end
+    return val
+end
+
+function get_2D_model2(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    f = 0.0
+    x = p[1]
+    y = p[2]
+    if x > 1.0 || x < 0.0 || y > 1.0 || y < 0.0
+        return f
+    end
+
+    if y <= x && y <= (1.0 - x)
+        f = 2.0 * y
+    end
+    if y <= x && y >= (1.0 - x)
+        f = 1.0 - 2.0 * (x - 0.5)
+    end
+    if y >= x && y >= (1.0 - x)
+        f = 1.0 - 2.0 * (y - 0.5)
+    end
+    if y >= x && y <= (1.0 - x)
+        f = 2.0 * x
+    end
+    return f
+end
+
+function get_2D_model2_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+
+    if y < x && y < (1.0 - x)
+        grad[1] = 0.0
+        grad[2] = 2.0
+    end
+    if y < x && y > (1.0 - x)
+        grad[1] = -2.0
+        grad[2] = 0.0
+    end
+    if y > x && y > (1.0 - x)
+        grad[1] = 0.0
+        grad[2] = -2.0
+    end
+    if y > x && y < (1.0 - x)
+        grad[1] = 2.0
+        grad[2] = 0.0
+    end
+    return grad
+end
+
+# Franke's CLIFF Function F2 (Cliff function) from
+# R. Renka, R. Brown, Algorithm 792: accuracy test of ACM algorithms for interpolation of scattered data in the plane, ACM Transactions on Mathematical Software (TOMS), Vol.25, No.1,1999
+function get_2D_model3(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    x = p[1]
+    y = p[2]
+    f = (tanh(9.0 * (y - x)) + 1.0) / (tanh(9.0)+ 1.0)
+    return f
+end
+
+function get_2D_model3_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+
+    grad[1] = (-9.0 / (tanh(9.0)+ 1.0)) * (sech(9.0 * (x - y)))^2
+    grad[2] = (9.0 / (tanh(9.0)+ 1.0)) * (sech(9.0 * (x - y)))^2
+    return grad
+end
+
+# Function F10 from
+# R. Renka, R. Brown, Algorithm 792: accuracy test of ACM algorithms for interpolation of scattered data in the plane, ACM Transactions on Mathematical Software (TOMS), Vol.25, No.1,1999
+function get_2D_model4(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    x = p[1]
+    y = p[2]
+    d = sqrt((80.0 * x - 40.0)^2 + (90.0 * y - 45.0)^2)
+    f = exp(-0.04 * d) * cos(0.15 * d)
+    return f
+end
+
+function get_2D_model4_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    d = sqrt((80.0 * x - 40.0)^2 + (90.0 * y - 45.0)^2)
+    if d > 0.0
+        grad[1] = exp(-0.04 * d) * (-(960.0 * x - 480.0) * sin(0.15 * d) - (256.0 * x - 128.0) * cos(0.15 * d)) / d
+        grad[2] = exp(-0.04 * d) * (-(1215.0 * y - 607.5) * sin(0.15 * d) - (324.0 * x - 162.0) * cos(0.15 * d)) / d
+    end
+    return grad
+end
+
+# Function F2 from
+# S. De Marchi, A. Martinez, E. Perracchione, Fast and stable rational RBF-based partition of unity interpolation. Journal of Computational and Applied Mathematics, 2018.
+function get_2D_model5(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    x = p[1]
+    y = p[2]
+    f = (x + y - 1.0)^7
+    return f
+end
+
+function get_2D_model5_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    grad[1] = grad[2] = 7.0 * (x + y - 1.0)^6
+    return grad
+end
+
+function get_2D_model6(p::Vector{Float64})
+#  (x,y) \in [-1;1][-1;1]
+    x = p[1]
+    y = p[2]
+    f = sin(4.0 * sqrt(x^2 + y^2))
+    return f
+end
+
+function get_2D_model6_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    d = sqrt(x^2 + y^2)
+    if d > 0.0
+        grad[1] = 4.0 * x * cos(4.0 * d) / d
+        grad[2] = 4.0 * y * cos(4.0 * d) / d
+    end
+    return grad
+end
+
+function get_2D_model10(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    return 1.0
+end
+
+function get_2D_model11(p::Vector{Float64})
+#  (x,y) \in [0;1][0;1]
+    val = 0.0
+    x = p[1]
+    y = p[2]
+    d = (x - 0.5)^2 + (y - 0.5)^2
+    r2 = 0.16 * (1.0 + 100.0 * eps(1.0))
+    if d <= r2
+        val = sqrt(abs(0.16 - d))
+    end
+    return val
+end
+
+function get_2D_model12(p::Vector{Float64})
+#  (x,y) \in [-4;4][-2;2]
+    val = 0.0
+    x = p[1]
+    y = p[2]
+    d = x^2 + y^2
+    r2 = 1.0 + 100.0 * eps(1.0)
+    if d <= r2
+        val = sqrt(abs(1.0 - d))
+    end
+    d = (x - 2.5)^2 + y^2
+    if d <= r2
+        val = sqrt(abs(1.0 - d))
+    end
+    d = (x + 2.5)^2 + y^2
+    if d <= r2
+        val = sqrt(abs(1.0 - d))
+    end
+    return val
+end
+
+function get_2D_model12_grad(p::Vector{Float64})
+#  (x,y) \in [-4;4][-2;2]
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    d = x^2 + y^2
+    r2 = 1.0 + 100.0 * eps(1.0)
+    if d <= r2 && abs(1.0 - d) > eps()
+        grad[1] =  -x / sqrt(abs(1.0 - d))
+        grad[2] =  -y / sqrt(abs(1.0 - d))
+    end
+    d = (x - 2.5)^2 + y^2
+    if d <= r2 && abs(1.0 - d) > eps()
+        grad[1] =  -(x - 2.5) / sqrt(abs(1.0 - d))
+        grad[2] =  -y / sqrt(abs(1.0 - d))
+    end
+    d = (x + 2.5)^2 + y^2
+    if d <= r2 && abs(1.0 - d) > eps()
+        grad[1] =  -(x + 2.5) / sqrt(abs(1.0 - d))
+        grad[2] =  -y / sqrt(abs(1.0 - d))
+    end
+    return grad
+end
+
+function get_2D_model13(p::Vector{Float64})
+# Franke's EXPONENTIAL f1
+# R. Renka, R. Brown, Algorithm 792: accuracy test of ACM algorithms for interpolation of scattered data in the plane, ACM Transactions on Mathematical Software (TOMS), Vol.25, No.1,1999
+# https://dl.acm.org/doi/10.1145/305658.305745
+#  (x,y) \in [0;1][0;1]
+    x = p[1]
+    y = p[2]
+    val = 0.75*exp(-((9.0* x - 2.)^2 + (9.0*y - 2.)^2)/4.0) +
+          0.75*exp(-((9.0*x + 1.0)^2)/49. - (9.0*y + 1.0)/10.0) +
+          0.5*exp(-((9.0*x - 7.)^2 + (9.0*y - 3.)^2)/4.0) -
+          0.2*exp(-(9.0*x - 4.)^2 - (9.0*y - 7.)^2)
+    return val
+end
+
+function get_2D_model13_grad(p::Vector{Float64})
+    #  (x,y) \in [0;1][0;1]
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    grad[1] = -0.27551*(9.0*x + 1.0)*exp(0.1*(-9.0*y - 1.0) - (1.0/49.0)*(9.0*x + 1.0)^2) -
+               2.25*(9.0*x - 7.)*exp(0.25*(-(9.0*x - 7.)^2 - (9.0*y - 3.)^2)) +
+               3.6*(9.0*x - 4.)*exp(-(9.0*x - 4.)^2 - (9.0*y - 7.)^2) -
+               3.375*(9.0*x - 2.)*exp(0.25*(-(9.0*x - 2.)^2 - (9.0*y - 2.)^2))
+    grad[2] = -0.675*exp(0.1*(-9.0*y - 1.0) - (1.0/49.0)*(9.0*x + 1.0)^2) +
+               3.6*(9.0*y - 7.)*exp(-(9.0*x - 4.)^2 - (9.0*y - 7.)^2) -
+               2.25*(9.0*y - 3.)*exp(0.25*(-(9.0*x - 7.)^2 - (9.0*y - 3.)^2)) -
+               3.375*(9.0*y - 2.)*exp(0.25*(-(9.0*x - 2.)^2 - (9.0*y - 2.)^2))
+    return grad
+end
+
+# Rosenbrock function
+# https://en.wikipedia.org/wiki/Rosenbrock_function
+function get_2D_model14(p::Vector{Float64})
+#  (x,y) \in [-2;2]x[-1;3]
+# max f_orig = 2500
+    x = p[1]
+    y = p[2]
+    f = ((1.0 - x)^2 + 100.0*(y - x^2)^2) / 2500.0
+    return f
+end
+
+function get_2D_model14_grad(p::Vector{Float64})
+    grad = [0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    grad[1] = (400.0*x^3 + x*(2.0 - 400.0*y) - 2.0) / 2500.0
+    grad[2] = 200.0*(y - x^2) / 2500.0
+    return grad
+end
+
+########### 3D #########
+
+function get_3D_model1(p::Vector{Float64})
+    r = p[1] + p[2] + p[3]
+    val = -1.0
+    if(p[1] < 0.0 || p[2] < 0.0 || p[3] < 0.0 || r > 1.0)
+        return val
+    end
+    return r
+end
+
+function get_3D_model1_grad()
+    return [1.0; 1.0; 1.0]
+end
+
+function get_3D_model2(p::Vector{Float64})
+    return p[3]
+end
+
+function get_3D_model2_grad()
+    return [0.0; 0.0; 1.0]
+end
+
+function get_3D_model3(p::Vector{Float64})
+# This is model3 with misprint corrected
+# T. Foley, Interpolation and approximation of 3-D and 4-D scattered data, Comput. Math. Appl., Vol.13, No.8, 1987.
+# https://www.sciencedirect.com/science/article/pii/0898122187900435
+# G_1(x,y,z), (x,y,z) \in [0;1][0;1][0;1]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+
+    val = 0.75*exp(-16.0*((x - 0.25)^2 + (y - 0.25)^2 + (z - 0.5)^2)) +
+          0.50*exp(-10.0*((x - 0.25)^2 + (y - 0.25)^2)) +
+          0.50*exp(-10.0*((x - 0.75)^2 + (y - 0.125)^2 + (z - 0.5)^2)) -
+          0.25*exp(-20*((x - 0.75)^2 + (y - 0.75)^2))
+    return val
+end
+
+function get_3D_model3_grad(p::Vector{Float64})
+# This is model3 with misprint corrected
+    grad = [0.0; 0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    grad[1] = -24.0*exp(-16.0*((-0.25 + x)^2 + (-0.25 + y)^2 + (-0.25 + z)^2))*(-0.25 + x) -
+              10.0*exp(-10.0*((-0.25 + x)^2 + (-0.25 + y)^2))*(-0.25 + x) -
+              10.0*exp(-10.0*((-0.75 + x)^2 + (-0.125 + y)^2 + (-0.5 + z)^2))*(-0.75 + x) +
+              10.0*exp(-20.0*((-0.75 + x)^2 + (-0.75 + y)^2))*(-0.75 + x)
+    grad[2] = -24.0*exp(-16.0*((-0.25 + x)^2 + (-0.25 + y)^2 + (-0.25 + z)^2))*(-0.25 + y) -
+              10.0*exp(-10.0*((-0.25 + x)^2 + (-0.25 + y)^2))*(-0.25 + y) -
+              10.0*exp(-10.0*((-0.75 + x)^2 + (-0.125 + y)^2 + (-0.5 + z)^2))*(-0.125 + y) +
+              10.0*exp(-20.0*((-0.75 + x)^2 + (-0.75 + y)^2))*(-0.75 + y)
+    grad[3] = -24.0*exp(-16.0*((-0.25 + x)^2 + (-0.25 + y)^2 + (-0.25 + z)^2))*(-0.5 + z) -
+              10.0*exp(-10.0*((-0.75 + x)^2 + (-0.125 + y)^2 + (-0.5 + z)^2))*(-0.5 + z)
+    return grad
+end
+
+function get_3D_model4(p::Vector{Float64})
+# T. Foley, Interpolation and approximation of 3-D and 4-D scattered data, Comput. Math. Appl., Vol.13, No.8, 1987.
+# https://www.sciencedirect.com/science/article/pii/0898122187900435
+# G_2(x,y,z), (x,y,z) \in [0;1][0;1][0;1]
+    val = get_3D_model3(p)
+    if val < 0.0
+        val = 0.0
+    end
+    if val > 0.5
+        val = 0.5
+    end
+    return val
+end
+
+function get_3D_model4_grad(p::Vector{Float64})
+    grad = get_3D_model3_grad(p)
+    val = get_3D_model3(p)
+    if val < 0.0
+        grad = [0.0; 0.0; 0.0]
+    end
+    if val > 0.5
+        grad = [0.0; 0.0; 0.0]
+    end
+    return grad
+end
+
+# Function F2 (Cliff function) from
+# R. Renka, Multivariate interpolation of large sets of scattered data. ACM Transactions on Mathematical Software, Vol.14, No.2, 1988.
+# http://web.eecs.utk.edu/research/imp/tools/interp/
+function get_3D_model5(p::Vector{Float64})
+# Franke's SADDLE F3
+#  (x,y,z) \in [0;1][0;1][0;1]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    f = (1.25 + cos(5.4*y))*cos(6.0*z)/(6.0 + 6.0*(3.0*x - 1.0)^2)
+    return f
+end
+
+function get_3D_model5_grad(p::Vector{Float64})
+    grad = [0.0; 0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    grad[1] = -((108.0*x - 36.0)*(cos(5.4*y) + 1.25)*cos(6.0*z))/(6.0 + 6.0*(3.0*x - 1.0)^2)^2
+    grad[2] = -(5.4*sin(5.4*y)*cos(6.0*z))/(6.0 + 6.0*(3.0*x - 1.0)^2)
+    grad[3] = -(cos(5.4*y) + 1.25)*sin(6.0*z)/(1.0 + (3.0*x - 1.0)^2)
+    return grad
+end
+
+# ROBERT E. BARNHILL and SARAH E. STEAD, Multistage trivariate surfaces, The Rocky Mountain Journal of Mathematics, Vol. 14, No. 1, 1984.
+# https://www.jstor.org/stable/44236789?seq=1
+function get_3D_model6(p::Vector{Float64})
+#  (x,y,z) \in [0;1][0;1][0;1]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    f = cos(π*x)*cos(y - 0.5)*sin(π*(z - 0.5))
+    return f
+end
+
+function get_3D_model6_grad(p::Vector{Float64})
+    grad = [0.0; 0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    grad[1] = -π*sin(π*x)*cos(y - 0.5)*sin(π*(z - 0.5))
+    grad[2] = -cos(π*x)*sin(y - 0.5)*sin(π*(z - 0.5))
+    grad[3] = π*cos(π*x)*cos(y - 0.5)*cos(π*(z - 0.5))
+    return grad
+end
+
+# Generalized Rosenbrock function
+# https://en.wikipedia.org/wiki/Rosenbrock_function
+function get_3D_model7(p::Vector{Float64})
+#  (x,y,z) \in [-2.5;2.5]x[-2.5;2.5]x[0;16000]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    f = 100.0*(y - x^2)^2 + (1.0 - x)^2 + 100.0*(z - y^2)^2 + (1.0 - y)^2
+    return f
+end
+
+function get_3D_model7_grad(p::Vector{Float64})
+    grad = [0.0; 0.0; 0.0]
+    x = p[1]
+    y = p[2]
+    z = p[3]
+    grad[1] = 400.0*x^3 + x*(2.0 - 400.0*y) - 2.0
+    grad[2] = -2.0 - 200.0*x^2 + 400.0*y^3 + y*(202.0 - 400.0*z)
+    grad[3] = 200.0*(z - y^2)
+    return grad
+end
