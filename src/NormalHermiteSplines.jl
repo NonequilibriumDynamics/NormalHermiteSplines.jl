9e57d67a5c26a612d469aa54db7f1ceee4dd5556
IgorKohan <nettvor@gmail.com>
2020-09-06 23:27:13 +0200

    .

--- src/NormalHermiteSplines.jl
+++ src/NormalHermiteSplines.jl
@@ -352,8 +352,8 @@ end
 """
 `estimate_accuracy(spline::NormalSpline{T, RK}) where {T <: AbstractFloat, RK <: ReproducingKernel_0}`
 
-Estimate accuracy of interpolation by calculating an estimation of the number of significant digits in the
-interpolation result.
+Estimate accuracy of interpolation by calculating an estimation of the number of significant digits
+in the function value interpolation result.
 # Arguments
 - `spline`: the `NormalSpline` object returned by `construct` or `interpolate` function.
 
